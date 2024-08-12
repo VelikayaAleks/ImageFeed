646772bd54713ff25b8493f347ba6d74f4bdd981
VelikayaAleks <sashuunka@yandex.ru>
2024-08-12 07:26:06 +0300

    Bugs fixed

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -3,8 +3,8 @@ import Foundation
 
 
 let defaultBaseURL = URL(string: "https://api.unsplash.com")
-let secretKey = "8IRDsodGWoiWe_9DLdGKq3a4KhAV-xsxgvBErFkWtFk"
-let accessKey = "V1UdTFRheeAy7E6BAjXpZhssohQasjEhcuFZwmYsYtM"
+let accessKey = "8IRDsodGWoiWe_9DLdGKq3a4KhAV-xsxgvBErFkWtFk"
+let secretKey = "V1UdTFRheeAy7E6BAjXpZhssohQasjEhcuFZwmYsYtM"
 let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
 let accessScope = "public+read_user+write_likes"
 let grantType = "authorization_code"
